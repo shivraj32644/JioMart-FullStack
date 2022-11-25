75dd9166ec1aee64930d4e22d75082cb014ebccd
shivraj32644 <shivraj32644@gmail.com>
2022-11-25 17:18:56 +0530

    prduct api

--- Backend/Routes/product.routes.js
+++ Backend/Routes/product.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { addProduct, getProducts } from "../Controller/product.controller.js";
+import { getProductById, getProducts } from "../Controller/product.controller.js";
 
 export const productRoute = Router();
 
 productRoute.get("/", getProducts);
-productRoute.post("/product",addProduct)
+productRoute.get("/:id", getProductById);
+// productRoute.post("/product",addProduct)
