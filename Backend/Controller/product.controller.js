3fc4c0933af9609f8aae9156f9eb230268677fbb
shivraj32644 <shivraj32644@gmail.com>
2022-11-25 14:13:32 +0530

    prduct api

--- Backend/Controller/product.controller.js
+++ Backend/Controller/product.controller.js
@@ -1,54 +1,48 @@
-import { productModel } from "../Models/product.model.js"
+import { productModel } from "../Models/product.model.js";
 
-
-export const getProducts = async(req,res) => {
-    try {
-        let {q,sort,limit,page,order} = req.query
-        let filter = req.query;
-        let data =  await productModel.find();
-        
-        if (page && limit) {
-            limit = Number(limit);
-            data = await productModel.find().limit(limit).skip(limit*(page-1))
-        }
-        if (filter) {
-            let val = Object.values(filter)
-            let temp = Number(val);
-            if (isNaN(temp)) {
-                data = await productModel.find(filter)
-            }
-            else {
-                let key = Object.keys(filter)[0]
-                data = await productModel.find({[key]:temp})
-            }
-        }
-        if (q) {
-            console.log("object");
-            await productModel.createIndexes({item_name:"text"})
-            data = await productModel.find({ $text: { $search: 'Maggi' } })
-        }
-        if (sort && order) {
-            order = order === 'asc' ? 1 : -1;
-            data = await productModel.find().sort({[sort]:order})
-        }
-
-        return res.status(200).send(data)
-        
-    } catch (error) {
-        return res.status(500).send({
-            status: "Failed",
-            message:"Server Error"
-        })
-    }
-}
-
-export const addProduct = async (req, res) =>{
+export const getProducts = async (req, res) => {
+  try {
+    let data = await productModel.find();
+    const length = await productModel.find().count();
+    let page = parseInt(req.query.page) - 1 || 0;
+    const limit = parseInt(req.query.limit) || length;
+    const search = req.query.q || "";
+    let sort = req.query.sort || "";
+    let order = req.query.order || "";
+    let filter = req.query || {};
+    delete filter.sort;
+    delete filter.limit;
+    delete filter.q;
+    delete filter.page;
+    delete filter.order;
     
-}
+    console.log(filter);
+    // if()
+    filter = { item_final_price: {  $gte: 2000, $lte: 4000 } } 
+    let sortBy = {};
+    if (order === "asc") {
+      sortBy[sort] = 1;
+    } else if (order === "desc") {
+      sortBy[sort] = -1;
+    }
+
+    data = await productModel
+      .find({ ...filter, item_name: { $regex: search, $options: "i" } })
+      .sort(sortBy)
+      .skip(page * limit)
+      .limit(limit);
+      return res.status(200).send(data);
 
 
+  } catch (error) {
+    return res.status(500).send({
+      status: "Failed",
+      message: "Server Error",
+    });
+  }
+};
 
-
+export const addProduct = async (req, res) => {};
 
 /*
 
@@ -111,6 +105,36 @@ cart = [
 cartModel.find({ userID: akshansh })
 
 => 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // if (q) {
+    //     console.log(q);
+    //     productModel.createIndexes({ item_category: 'text' }, (err) => {
+    //         console.log(err)
+    //     })
+
+    //     data = await productModel.find({ $text: { $search: q }  })
+    //     console.log(data);
+
+    //     // productModel.createIndexes({title:"text"})
+    //     //      data = await productModel.find({ $text: { $search: q } })
+
+    //     // ! db.blogs.createIndex({ title: 'text', content: 'text' })
+    //     // ! db.blogs.find({ $text: { $search: 'mongodb intro' }  })
+
+    // }
 
 
 
