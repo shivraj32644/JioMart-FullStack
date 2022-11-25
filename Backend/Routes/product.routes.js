05bdbddab7435fbdef0b61f830c9d621af7f1d33
Shivraj Dangi <108886442+shivraj32644@users.noreply.github.com>
2022-11-25 16:15:50 +0530

    Delete Backend directory

--- Backend/Routes/product.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { addProduct, getProducts } from "../Controller/product.controller.js";
-
-export const productRoute = Router();
-
-productRoute.get("/", getProducts);
-productRoute.post("/product",addProduct)
--- Backend/Routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-import {Router} from 'express'
-import { GetUser } from '../Controller/user.controller.js';
-
-export const UserRouter = Router();
-
-
-UserRouter.get('/:id',GetUser);
