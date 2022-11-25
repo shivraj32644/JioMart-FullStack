0f78d0a105d213f6ef8f5b7d72572bfa7840056c
shivraj32644 <shivraj32644@gmail.com>
2022-11-25 16:10:50 +0530

    api completed

--- Backend/Controller/product.controller.js
+++ Backend/Controller/product.controller.js
@@ -1,124 +1,122 @@
-import { productModel } from "../Models/product.model.js";
+// import { productModel } from "../Models/product.model.js";
 
-export const getProducts = async (req, res) => {
-  try {
-    let data = await productModel.find();
-    const length = await productModel.find().count();
-    let page = parseInt(req.query.page) - 1 || 0;
-    const limit = parseInt(req.query.limit) || length;
-    const search = req.query.q || "";
-    let sort = req.query.sort || "";
-    let order = req.query.order || "";
-    let filter = req.query || {};
-    delete filter.sort;
-    delete filter.limit;
-    delete filter.q;
-    delete filter.page;
-    delete filter.order;
+// export const getProducts = async (req, res) => {
+//   try {
+//     let data = await productModel.find();
+//     const length = await productModel.find().count();
+//     let page = parseInt(req.query.page) - 1 || 0;
+//     const limit = parseInt(req.query.limit) || length;
+//     const search = req.query.q || "";
+//     let sort = req.query.sort || "";
+//     let order = req.query.order || "";
+//     let filter = req.query || {};
+//     delete filter.sort;
+//     delete filter.limit;
+//     delete filter.q;
+//     delete filter.page;
+//     delete filter.order;
     
-    // console.log(filter);
-    // var allKeys = Object.keys(filter)
-    // console.log("All Keys", allKeys[0].split("$"))
-    // console.log("All Keys", allKeys[1].split("$"))
+//     // console.log(filter);
+//     // var allKeys = Object.keys(filter)
+//     // console.log("All Keys", allKeys[0].split("$"))
+//     // console.log("All Keys", allKeys[1].split("$"))
     
-    // if (allKeys.includes("lte") && allKeys.includes("gte")) {
+//     // if (allKeys.includes("lte") && allKeys.includes("gte")) {
       
       
-    //   const key = allKeys[0]
-    //   filter = { [key]: {  $gte: 2000, $lte: 4000 } }
-    // }
-    // else {
+//     //   const key = allKeys[0]
+//     //   filter = { [key]: {  $gte: 2000, $lte: 4000 } }
+//     // }
+//     // else {
       
-    // }
-    // if()
+//     // }
+//     // if()
      
-    let sortBy = {};
-    if (order === "asc") {
-      sortBy[sort] = 1;
-    } else if (order === "desc") {
-      sortBy[sort] = -1;
-    }
+//     let sortBy = {};
+//     if (order === "asc") {
+//       sortBy[sort] = 1;
+//     } else if (order === "desc") {
+//       sortBy[sort] = -1;
+//     }
 
-    data = await productModel.find({ ...filter, item_name: { $regex: search, $options: "i" } })
-      .sort(sortBy)
-      .skip(page * limit)
-      .limit(limit);
-      return res.status(200).send(data);
-
-
-  } catch (error) {
-    return res.status(500).send({
-      status: "Failed",
-      message: "Server Error",
-    });
-  }
-};
-
-export const addProduct = async (req, res) => {};
-
-/*
-
-user = [
-    {
-        _id: akshansh
-        cartItem:[item1,item2item3]
-    },
-    {
-    _id:shivraj
-    },
-    {
-    _id:abhi
-    },
-    {
-    _id:jay
-    },
-]
-
-// ===============================================
-1
-let user = user.find()
-
-user.cartItem
-
-let newItem = "";
-
-user.cartItem.push(newItem);
-
-// ==================================
-2
-cart = [
-    {
-        cartDetail: "same",
-        userId:akshansh
-    },
-    {
-        cartDetail: "same",
-        userId:shivraj
-    },
-    {
-        cartDetail: "item2",
-        userId:akshansh
-    },
-    {
-        cartDetail: "",
-        userId:jay
-    },
-    {
-        cartDetail: "",
-        userId:abhi
-    },
-    {
-
-    },
+//     data = await productModel.find({ ...filter, item_name: { $regex: search, $options: "i" } })
+//       .sort(sortBy)
+//       .skip(page * limit)
+//       .limit(limit);
+//       return res.status(200).send(data);
 
 
-]
+//   } catch (error) {
+//     return res.status(500).send({
+//       status: "Failed",
+//       message: "Server Error",
+//     });
+//   }
+// };
 
-cartModel.find({ userID: akshansh })
+// export const addProduct = async (req, res) => {};
 
-=> 3
+// /*
+
+// user = [
+//     {
+//         _id: akshansh
+//         cartItem:[item1,item2item3]
+//     },
+//     {
+//     _id:shivraj
+//     },
+//     {
+//     _id:abhi
+//     },
+//     {
+//     _id:jay
+//     },
+// ]
+
+// // ===============================================
+// 1
+// let user = user.find()
+
+// user.cartItem
+
+// let newItem = "";
+
+// user.cartItem.push(newItem);
+
+// // ==================================
+// 2
+// cart = [
+//     {
+//         cartDetail: "same",
+//         userId:akshansh
+//     },
+//     {
+//         cartDetail: "same",
+//         userId:shivraj
+//     },
+//     {
+//         cartDetail: "item2",
+//         userId:akshansh
+//     },
+//     {
+//         cartDetail: "",
+//         userId:jay
+//     },
+//     {
+//         cartDetail: "",
+//         userId:abhi
+//     },
+//     {
+
+//     },
 
 
+// ]
+
+// cartModel.find({ userID: akshansh })
+
+// => 3
 
 
 
@@ -131,23 +129,25 @@ cartModel.find({ userID: akshansh })
 
 
 
-    // if (q) {
-    //     console.log(q);
-    //     productModel.createIndexes({ item_category: 'text' }, (err) => {
-    //         console.log(err)
-    //     })
-
-    //     data = await productModel.find({ $text: { $search: q }  })
-    //     console.log(data);
-
-    //     // productModel.createIndexes({title:"text"})
-    //     //      data = await productModel.find({ $text: { $search: q } })
-
-    //     // ! db.blogs.createIndex({ title: 'text', content: 'text' })
-    //     // ! db.blogs.find({ $text: { $search: 'mongodb intro' }  })
-
-    // }
 
 
+//     // if (q) {
+//     //     console.log(q);
+//     //     productModel.createIndexes({ item_category: 'text' }, (err) => {
+//     //         console.log(err)
+//     //     })
 
-*/
+//     //     data = await productModel.find({ $text: { $search: q }  })
+//     //     console.log(data);
+
+//     //     // productModel.createIndexes({title:"text"})
+//     //     //      data = await productModel.find({ $text: { $search: q } })
+
+//     //     // ! db.blogs.createIndex({ title: 'text', content: 'text' })
+//     //     // ! db.blogs.find({ $text: { $search: 'mongodb intro' }  })
+
+//     // }
+
+
+
+// */
--- Backend/configs/db.connect.js
+++ Backend/configs/db.connect.js
@@ -1,22 +1,22 @@
-import mongoose from "mongoose"
-import * as dotenv from 'dotenv'
-dotenv.config();
+// import mongoose from "mongoose"
+// import * as dotenv from 'dotenv'
+// dotenv.config();
 
-const username = process.env.mongoUserName;
-const password = process.env.mongoPassword;
+// const username = process.env.mongoUserName;
+// const password = process.env.mongoPassword;
 
 
-export const connectDataBase = () => {
-    return new Promise((resolve, reject) => {
-        mongoose.connect(`mongodb+srv://${username}:${password}@database.5lmxkzm.mongodb.net/?retryWrites=true&w=majority`, (err) => {
-            if (err) {
-                reject("Database Connect Failed")
-            }
-            else {
-                resolve();
-            }
-        })
-    })
-}
+// export const connectDataBase = () => {
+//     return new Promise((resolve, reject) => {
+//         mongoose.connect(`mongodb+srv://${username}:${password}@database.5lmxkzm.mongodb.net/?retryWrites=true&w=majority`, (err) => {
+//             if (err) {
+//                 reject("Database Connect Failed")
+//             }
+//             else {
+//                 resolve();
+//             }
+//         })
+//     })
+// }
 
 
