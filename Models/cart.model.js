ac87dd5674d5a963f543b3bf0e48170fc81e16c6
shivraj32644 <shivraj32644@gmail.com>
2022-11-27 16:34:02 +0530

    Cart Schema Changed

--- Models/cart.model.js
+++ Models/cart.model.js
@@ -7,7 +7,8 @@ const cartSchema = new mongoose.Schema({
     item_discount : String,
     item_tag_name : String,
     item_disc_price : String,
-    item_quantity : Number,
+    item_quantity: Number,
+    "item_Id":String,
     user_Id : {type : String, required : true},
 },
 {
