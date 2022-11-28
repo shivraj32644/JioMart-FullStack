36be013089c6129b002009845dcab34f801411fc
abhishekpandey108 <107405196+abhishekpandey108@users.noreply.github.com>
2022-11-28 20:39:43 +0530

    3

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -1,7 +1,7 @@
-import {Router} from 'express'
+import {Router} from 'express';
 import { GetUser } from '../Controller/user.controller.js';
 
 export const UserRouter = Router();
 
 
-UserRouter.get('/:id',GetUser)
+UserRouter.get('/:id',GetUser);
